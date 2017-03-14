11137f15f7e480c9c79a2019b93696519aef0819
hacktiv8 <hacktiv8@hacktiv8-4.local>
2017-03-14 10:14:59 +0700

    Create Model

--- server/app.js
+++ server/app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/quotes')
 var app = express();
 
 // view engine setup
@@ -22,6 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors())
 app.use('/', index);
 app.use('/users', users);
 
---
+++ server/models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+
+var quoteSchema = new Schema ({
+  imgurl = String,
+},{
+  timestamps: true
+})
+
+var Quote = mongoose.model('Quote', quoteSchema)
+module.exports = Quote
